dbcaaefe1099a9215b6bd84580142adcdd5732c9
vickeetran <ptran.vicki@gmail.com>
2016-05-13 21:18:15 -0700

    Set up mysql Tables

--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,34 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  user_id int NOT NULL auto_increment,
+  PRIMARY KEY (user_id),
+  username varchar(25) DEFAULT NULL,
+  UNIQUE(username)
 );
 
-/* Create other tables and define schemas for them here! */
+
+CREATE TABLE rooms (
+  room_id int NOT NULL auto_increment,
+  PRIMARY KEY (room_id),
+  room varchar(25) DEFAULT NULL,
+  UNIQUE(room)
+);
 
 
+CREATE TABLE messages (
+  chat_id int NOT NULL auto_increment,
+  PRIMARY KEY (chat_id),
+  posted_on datetime NOT NULL,
+  user_id int NOT NULL,
+  room_id int NOT NULL,
+  message text NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (room_id) REFERENCES rooms(room_id)
+);
 
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
